684103fae3eba521e1843b26dd77866c2bb7ecf7
Flobul <flobul.jeedom@gmail.com>
2021-05-04 09:53:36 +0200

    adding remove button

--- desktop/js/homeconnect.js
+++ desktop/js/homeconnect.js
@@ -128,7 +128,8 @@ function addCmdToTable(_cmd) {
   tr += '<td>';
   if (is_numeric(_cmd.id)) {
     tr += '<a class="btn btn-default btn-xs cmdAction expertModeVisible" data-action="configure"><i class="fa fa-cogs"></i></a> ';
-    tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fa fa-rss"></i> {{Tester}}</a>';
+    tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fa fa-rss"></i> {{Tester}}</a> ';
+    tr += '<a class="btn btn-default btn-xs cmdAction" data-action="remove"><i class="fas fa-minus-circle"></i></a>';
   }
   tr += '</tr>';
   $('#table_cmd tbody').append(tr);
