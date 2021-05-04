6437aaeff2c8c7f99ad3c98c70a82af968498b0a
Flobul <flobul.jeedom@gmail.com>
2021-05-04 09:59:42 +0200

    adding cells: type and parameter

--- desktop/php/homeconnect.php
+++ desktop/php/homeconnect.php
@@ -160,7 +160,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 				<table id="table_cmd" class="table table-bordered table-condensed">
 					<thead>
 						<tr>
-							<th>{{Nom}}</th><th>{{Options}}</th><th>{{Action}}</th>
+							<th>{{Nom}}</th><th>{{Options}}</th><th>{{Type}}</th><th>{{Paramètres}}</th><th>{{Action}}</th>
 						</tr>
 					</thead>
 					<tbody>
