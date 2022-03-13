9201ab7b1c80d7ecb5598b20e9b2cb62b7062890
Flobul <46993341+Flobul@users.noreply.github.com>
2022-03-13 11:34:09 +0100

    Adding select to see/modify link between action and info cmd. Reorganization to follow jeedom v4.2 print.

--- desktop/js/homeconnect.js
+++ desktop/js/homeconnect.js
@@ -97,19 +97,19 @@ function addCmdToTable(_cmd) {
   if (!isset(_cmd.configuration)) {
     _cmd.configuration = {};
   }
-  var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">';
-  tr += '<td>';
-  tr += '<input class="cmdAttr form-control input-sm" data-l1key="id" style="display : none;">';
-  tr += '<div class="row">';
-  tr += '<div class="col-sm-4">';
-  tr += '<a class="cmdAction btn btn-default btn-sm" data-l1key="chooseIcon"><i class="fa fa-flag"></i> Icône</a>';
-  tr += '<span class="cmdAttr" data-l1key="display" data-l2key="icon" style="margin-left : 10px;"></span>';
-  tr += '</div>';
-  tr += '<div class="col-sm-8">';
-  tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" placeholder="{{Nom}}">';
-  tr += '</div>';
-  tr += '</div>';
-  tr += '</td>';
+  var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">'
+  tr += '<td class="hidden-xs">'
+  tr += '    <span class="cmdAttr" data-l1key="id" style="display:none;"></span>'
+  tr += '    <div class="input-group">'
+  tr += '        <input class="cmdAttr form-control input-sm roundedLeft" data-l1key="name" placeholder="{{Nom de la commande}}">'
+  tr += '        <span class="input-group-btn"><a class="cmdAction btn btn-sm btn-default" data-l1key="chooseIcon" title="{{Choisir une icône}}"><i class="fas fa-icons"></i></a></span>'
+  tr += '        <span class="cmdAttr input-group-addon roundedRight" data-l1key="display" data-l2key="icon" style="font-size:19px;padding:0 5px 0 0!important;background:var(--btn-default-color) !important";width:2%;></span>'
+  tr += '    </div>'
+  tr += '    <select class="cmdAttr form-control input-sm" data-l1key="value" style="display:none;margin-top:5px;max-width:50%" title="{{Commande info liée}}">'
+  tr += '        <option value="">{{Aucune}}</option>'
+  tr += '    </select>'
+  tr += '</td>'
+
   tr += '<td>';
   tr += '<input class="cmdAttr form-control type input-sm" data-l1key="type" value="action" disabled style="display:inline-block;margin-bottom:5px;max-width:120px;" />';
   tr += '<input class="cmdAttr form-control type input-sm" data-l1key="subType" value="action" disabled style="display:inline-block;margin-bottom:5px;max-width:120px;" />';
@@ -133,9 +133,18 @@ function addCmdToTable(_cmd) {
   }
   tr += '</tr>';
   $('#table_cmd tbody').append(tr);
-  $('#table_cmd tbody tr:last').setValues(_cmd, '.cmdAttr');
-  if (isset(_cmd.type)) {
-    $('#table_cmd tbody tr:last .cmdAttr[data-l1key=type]').value(init(_cmd.type));
-  }
+  var tr = $('#table_cmd tbody tr').last()
+  jeedom.eqLogic.buildSelectCmd({
+    id:  $('.eqLogicAttr[data-l1key=id]').value(),
+    filter: {type: 'info'},
+    error: function (error) {
+      $('#div_alert').showAlert({message: error.message, level: 'danger'})
+    },
+    success: function (result) {
+      tr.find('.cmdAttr[data-l1key=value]').append(result)
+      tr.setValues(_cmd, '.cmdAttr')
+      jeedom.cmd.changeType(tr, init(_cmd.subType))
+    }
+  })
   jeedom.cmd.changeType($('#table_cmd tbody tr:last'), init(_cmd.subType));
 }
