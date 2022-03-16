2a63e78d44aba8cb7c0c4d365b4fbbf04860567b
Flobul <46993341+Flobul@users.noreply.github.com>
2022-03-16 16:07:26 +0100

    Remove not used Options th

--- desktop/php/homeconnect.php
+++ desktop/php/homeconnect.php
@@ -176,7 +176,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 				<table id="table_cmd" class="table table-bordered table-condensed">
 					<thead>
 						<tr>
-							<th>{{Nom}}</th><th>{{Options}}</th><th>{{Type}}</th><th>{{Paramètres}}</th><th>{{Action}}</th>
+							<th>{{Nom}}</th><th>{{Type}}</th><th>{{Paramètres}}</th><th>{{Action}}</th>
 						</tr>
 					</thead>
 					<tbody>
